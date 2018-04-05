4470c691f025ab493ad5fa267b8e500124b88208
Jose Saldaña <josesaldanavi@gmail.com>
2018-04-04 22:37:24 -0500

    agregue introSound

--- JoseSaldana/Assets/UnityHangman/HangmanScript.cs
+++ JoseSaldana/Assets/UnityHangman/HangmanScript.cs
@@ -4,68 +4,85 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class HangmanScript : MonoBehaviour {
-	public string palabra;
+    public string palabra;
 
-	string palabraEscondida;
-	//se tiene que agregar la libreria UnityEngine.UI
-	public Text outputText;
-	public InputField inputText;
+    string palabraEscondida;
+    //se tiene que agregar la libreria UnityEngine.UI
+    public Text outputText;
+    public InputField inputText;
 
-	public AudioClip winSound;
-	public AudioClip successSound;
-	public AudioClip failSound;
-	public AudioSource camSource;
+    public AudioClip introSound;
+    public AudioClip winSound;
+    public AudioClip successSound;
+    public AudioClip failSound;
+    public AudioSource camSource;
 
-	bool endHangman;
-	// Use this for initialization
-	void Start () {
-		inputText.Select ();
-		foreach (char c in palabra) {
-			palabraEscondida += "*";
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(!endHangman){
-			runHangman ();
-		}
+    bool endHangman;
+    // Use this for initialization
+    void Start()
+    {
+        inputText.Select();
+        foreach (char c in palabra)
+        {
+            palabraEscondida += "*";
+            camSource.PlayOneShot(introSound);
+        }
+    }
 
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (!endHangman)
+        {
+            runHangman();
+        }
 
-	void runHangman(){
-		if (outputText.text != palabraEscondida) {
-			outputText.text = palabraEscondida;
-		}
-		//...metodo imput apretaste(enter)
-		if(Input.GetKeyDown(KeyCode.Return)&& !string.IsNullOrEmpty(inputText.text)){
-			string  letra = inputText.text.Substring(0,1);
-			if (palabra.Contains (letra)) {
-				string palabraTemporal = "";
-				for (int i = 0; i < palabra.Length; i++) {
-					if (palabra [i] == letra [0]) {
-						palabraTemporal += letra;
-					} else {
-						palabraTemporal += palabraEscondida [i];
-					}
-				}
-				palabraEscondida = palabraTemporal;
-				camSource.PlayOneShot (successSound);
-			} else {
-				camSource.PlayOneShot(failSound);
-			}
-			inputText.text = "";
-			inputText.Select ();
-			inputText.ActivateInputField ();
+    }
 
-		}
-		if(palabraEscondida == palabra){
-			camSource.PlayOneShot (winSound);
-			outputText.text = "Felicidades Ganaste";
-			inputText.gameObject.SetActive (false);
-			endHangman = true;
-		}
-	}
+    void runHangman()
+    {
+        if (outputText.text != palabraEscondida)
+        {
+            outputText.text = palabraEscondida;
+        }
+        //...metodo imput apretaste(enter)
+        if (Input.GetKeyDown(KeyCode.Return) && !string.IsNullOrEmpty(inputText.text))
+        {
+            string letra = inputText.text.Substring(0, 1);
+            if (palabra.Contains(letra))
+            {
+                string palabraTemporal = "";
+                for (int i = 0; i < palabra.Length; i++)
+                {
+                    if (palabra[i] == letra[0])
+                    {
+                        palabraTemporal += letra;
+                    }
+                    else
+                    {
+                        palabraTemporal += palabraEscondida[i];
+                    }
+                }
+                palabraEscondida = palabraTemporal;
+                camSource.PlayOneShot(successSound);
+            }
+            else
+            {
+                camSource.PlayOneShot(failSound);
+            }
+            inputText.text = "";
+            inputText.Select();
+            inputText.ActivateInputField();
+
+        }
+        if (palabraEscondida == palabra)
+        {
+            camSource.PlayOneShot(winSound);
+            outputText.text = "Felicidades Ganaste";
+            inputText.gameObject.SetActive(false);
+            endHangman = true;
+        }
+    }
 
 
 }
