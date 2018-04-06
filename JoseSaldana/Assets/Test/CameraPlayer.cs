f605b7d89bde439449fd69c2ff6cdb840a538ddf
Jose Saldaña <josesaldanavi@gmail.com>
2018-04-06 12:47:07 -0500

    agregue script de camara

---
+++ JoseSaldana/Assets/Test/CameraPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayer : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 distanciaP;
+
+	// Use this for initialization
+	void Start () {
+        distanciaP = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = player.transform.position + distanciaP;
+	}
+}
