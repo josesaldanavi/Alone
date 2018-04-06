b43c9b5d58bda6cfe5768f37f8c8321db1658f6f
joseSaldaña <josesaldanavi@gmail.com>
2018-04-05 22:02:31 -0500

    movimiento script

--- JoseSaldana/Assets/Test/Movimiento.cs
+++ JoseSaldana/Assets/Test/Movimiento.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Movimiento : MonoBehaviour {
-
+	public float speed;
+	public Vector3 limit;
 	// Use this for initialization
 	void Start () {
-		transform.position = new Vector3 (-1, 0, 0);
 		
 	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKey (KeyCode.A) && transform.position.x > -limit.x) {
+			transform.Translate(Vector3.left * speed *Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D) && transform.position.x < limit.x) {
+			transform.Translate(Vector3.right * speed *Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.W) && transform.position.z > -limit.z) {
+			transform.Translate (Vector3.forward * speed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)&& transform.position.z < limit.z) {
+			transform.Translate (Vector3.back * speed * Time.deltaTime);
+		}
 	}
 }
