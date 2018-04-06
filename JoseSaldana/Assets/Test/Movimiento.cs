43da443c25db2c63d0e3332683a3f9ec94addce0
Jose Saldaña <josesaldanavi@gmail.com>
2018-04-06 12:46:35 -0500

    agregue laberinto

--- JoseSaldana/Assets/Test/Movimiento.cs
+++ JoseSaldana/Assets/Test/Movimiento.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Movimiento : MonoBehaviour {
-	public float speed;
-	public Vector3 limit;
+    public float speed;
+
+    public AudioClip backgroudSound;
+    public AudioSource camSource;
 	// Use this for initialization
 	void Start () {
-		
+        camSource.PlayOneShot(backgroudSound);
 	}
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.A) && transform.position.x > -limit.x) {
-			transform.Translate(Vector3.left * speed *Time.deltaTime);
+
+		if (Input.GetKey (KeyCode.A)) {
+            transform.Rotate(new Vector3(0, -35, 0) * Time.deltaTime);
+        }
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(new Vector3 (0,35,0)*Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.D) && transform.position.x < limit.x) {
-			transform.Translate(Vector3.right * speed *Time.deltaTime);
-		}
-		if (Input.GetKey (KeyCode.W) && transform.position.z > -limit.z) {
+		if (Input.GetKey (KeyCode.W)) {
 			transform.Translate (Vector3.forward * speed * Time.deltaTime);
 		}
-		if (Input.GetKey (KeyCode.S)&& transform.position.z < limit.z) {
+		if (Input.GetKey (KeyCode.S)) {
 			transform.Translate (Vector3.back * speed * Time.deltaTime);
 		}
 	}
