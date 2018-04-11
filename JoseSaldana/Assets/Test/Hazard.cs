6e2dd8a945adcd01044642ced9379f2703277bd9
EthanChinchay <ethanchinchay@hotmail.com>
2018-04-10 22:08:33 -0500

    Laberinto Terminado :100:

---
+++ JoseSaldana/Assets/Test/Hazard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour {
+
+	void OnTriggerEnter (Collider other) {
+        Debug.Log("Entre en Trigger con " + other);
+        if (other.CompareTag("Player")) {
+            other.GetComponent<Movimiento> ().ResetPosition();
+        }
+	}
+}
--- JoseSaldana/Assets/Test/Movimiento.cs
+++ JoseSaldana/Assets/Test/Movimiento.cs
@@ -5,27 +5,40 @@ using UnityEngine.UI;
 
 public class Movimiento : MonoBehaviour {
     public float speed;
+    //Rigidbody playerRB;
+    Vector3 inicialPos;
 
     public AudioClip backgroudSound;
     public AudioSource camSource;
 	// Use this for initialization
 	void Start () {
+        inicialPos = transform.position;
+        //playerRB = GetComponent<Rigidbody>();
         camSource.PlayOneShot(backgroudSound);
 	}
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetKey (KeyCode.A)) {
+		if (Input.GetKey(KeyCode.A)) {
+            //playerRB.MovePosition(transform.position + (Vector3.left * speed * Time.deltaTime));
             transform.Rotate(new Vector3(0, -35, 0) * Time.deltaTime);
+            
         }
 		if (Input.GetKey (KeyCode.D)) {
 			transform.Rotate(new Vector3 (0,35,0)*Time.deltaTime);
+            
 		}
 		if (Input.GetKey (KeyCode.W)) {
-			transform.Translate (Vector3.forward * speed * Time.deltaTime);
-		}
+            //playerRB.MovePosition(transform.position+ (transform.forward * speed * Time.deltaTime));
+            transform.Translate (Vector3.forward * speed * Time.deltaTime);
+        }
 		if (Input.GetKey (KeyCode.S)) {
-			transform.Translate (Vector3.back * speed * Time.deltaTime);
-		}
+            transform.Translate (Vector3.back * speed * Time.deltaTime);
+        }
+        
 	}
+   public void ResetPosition()
+    {
+        transform.position = inicialPos;
+    }
 }
