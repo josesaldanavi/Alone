272d8064b594bfe4e600c527999bfbae270cf1bc
joseSaldana <josesaldanavi@gmail.com>
2018-04-19 22:13:00 -0500

    se tiene que arreglar

---
+++ JoseSaldana/Assets/FirstExample2D/BulletBevahaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBevahaviour : MonoBehaviour {
+    public float speed = 1;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Block"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
---
+++ JoseSaldana/Assets/FirstExample2D/TapDowmMovement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapDowmMovement : MonoBehaviour {
+    public float speed;
+    public float angularVelocity = 1;
+
+    public GameObject bullet;
+
+
+    public List<Color> colors = new List<Color>();
+    int colorIndex=0;
+
+    public SpriteRenderer spriteRenderer;
+    class Axis
+    {
+        public string name;
+        public KeyCode negative;
+        public KeyCode positive;
+        public Axis(string _name, KeyCode _negative,KeyCode _positive)
+        {
+            name = _name ;
+            negative = _negative;
+            positive = _positive;
+        }
+    }
+    List<Axis> axisList = new List<Axis>();
+
+	// Use this for initialization
+	void Start () {
+        spriteRenderer.color = colors[colorIndex];
+        axisList.Add(new Axis("Horizontal", KeyCode.A, KeyCode.D));
+        axisList.Add(new Axis("Vertical", KeyCode.S, KeyCode.W));
+        axisList.Add(new Axis("Arrow_H", KeyCode.LeftArrow, KeyCode.RightArrow));
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * GetAxis("Horizontal") * speed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.up * GetAxis("Vertical") * speed * Time.deltaTime, Space.World);
+        transform.Rotate(Vector3.back * GetAxis("Arrow_H") * angularVelocity * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            MoveColor();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        SpriteRenderer tempRender = Instantiate(bullet, transform.Find("Canon").position, transform.rotation).GetComponent<SpriteRenderer>();
+        tempRender.color = spriteRenderer.color;
+        Destroy(tempRenderer.gameObject, 2);
+    }
+    void MoveColor()
+    {
+        colorIndex = (colorIndex >= colors.Count -1) ? 0 : colorIndex + 1;
+        spriteRenderer.color = colors[colorIndex];
+    }
+    // metodo get axi creado dependiendo de los botones A nega y D posi
+    int GetAxis(string axisName)
+    {
+       Axis axis = axisList.Find(TargetJoint2D => Target.name == axisName);
+        int axisValue = 0;
+        if (Input.GetKey(axis.negative))
+        {
+            axisValue += -1;
+        }
+        if (Input.GetKey(axis.positive))
+        {
+            axisValue += 1;
+        }
+        return axisValue;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Block"))
+        {
+            Debug.Log("Block collision");
+        }
+    }
+}
