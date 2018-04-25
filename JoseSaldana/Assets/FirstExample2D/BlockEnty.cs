c16beab0f5c0741383baf03f2bf2ce479018b2ee
Marzelo <marzelozorogastuan@gmail.com>
2018-04-24 22:00:07 -0500

    Script para destruir el bloque :bomb:

---
+++ JoseSaldana/Assets/FirstExample2D/BlockEnty.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockEnty : MonoBehaviour {
+    public float lifePoints;
+	
+    public void DecreaseLife(int ammount)
+    {
+        //ammount es lo decreciente que hace las balas
+        lifePoints -= ammount;
+        if (lifePoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
